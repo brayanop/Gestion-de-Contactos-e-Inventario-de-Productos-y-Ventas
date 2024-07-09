6d962721376e97fa7006fa54e91e5f26fbcb687b
The Eddy Code <eddymg0110@gmail.com>
2024-07-09 03:07:22 -0500

    Mostrar listado general de contactos registrados hasta ese momento

--- EJERCICIO 1 - C1.cpp
+++ EJERCICIO 1 - C1.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-#include <string>
-#include <algorithm> 
 using namespace std;
+
 struct contactoEmail {
     string nombres_completos;
     char sexo;
@@ -10,45 +9,22 @@ struct contactoEmail {
     string email;
     string nacionalidad;
 };
-
-void intercambiar(contactoEmail& p1, contactoEmail& p2) {
-    contactoEmail temp = p1;
-    p1 = p2;
-    p2 = temp;
-}
-void ordenar(contactoEmail contacto[], int num) {
-    for (int i = 0; i < num-1; i++) {
-        for (int j = 0; j < num - i - 1; j++) {
-            size_t pos1 = contacto[j].email.find('@');
-            size_t pos2 = contacto[j + 1].email.find('@');
-            string dominio1 = contacto[j].email.substr(pos1 + 1);
-            string dominio2 = contacto[j + 1].email.substr(pos2 + 1);
-            if (dominio1 > dominio2) {
-                intercambiar(contacto[j], contacto[j + 1]);
-            }
-        }
-    }
-}
-
 int main() {
-    contactoEmail contacto[] = {
+    const int TAM = 3;
+    contactoEmail contacto[TAM] = {
         {"Brayan Mamani Choque", 'M', 16, "994 546 708", "brayan@gmail.com", "Peruano"},
-        {"Jose Llanos Marca", 'M', 18, "994 236 880", "Jhosep@yahoo.com", "Peruano"},
-        {"Eduardo Vilca Leon", 'M', 17, "954 986 784", "Eduardo@outlook.com", "Peruano"},
-        {"Marcos Sosa Mendosa", 'M', 24, "924 956 567", "Eduardo@gmail.com", "Peruano"},
-        {"Julia Carla Gonzales Quispe", 'F', 19, "954 985 991", "Eduardo@gmail.com", "Peruano"}
+        {"Jose Llanos Marca", 'M', 18, "994 236 880", "Jhosep@gmail.com", "Peruano"},
+        {"Eduardo Vilca Leon", 'M', 17, "954 986 784", "Eduardo@gmail.com", "Peruano"}
     };
-    int N = sizeof(contacto) / sizeof(contacto[0]);
-    ordenar(contacto, N);
-    cout << "Listado de contactos ordenados por dominio de correo:"<<endl;
-    for (int i = 0; i < N; ++i) {
-        cout<<"Nombre completo: " <<contacto[i].nombres_completos<<endl;
-        cout<<"Sexo: " <<contacto[i].sexo <<endl;
-        cout<<"Edad: " <<contacto[i].edad << endl;
-        cout<<"Telefono: " <<contacto[i].telefono <<endl;
-        cout<<"Correo electronico: "<<contacto[i].email <<endl;
-        cout<<"Nacionalidad: " <<contacto[i].nacionalidad <<endl;
-        cout<< endl;
+    int n = TAM ;
+    cout << " * * * * Mostrando lista general de contactos registrados * * * * "<< endl;
+    for (int i = 0; i < n; i++) {
+        cout << "Nombre: " << contacto[i].nombres_completos <<endl;
+        cout << "Sexo: " << contacto[i].sexo << endl;
+        cout << "Edad: " << contacto[i].edad << endl;
+        cout << "Telefono: " << contacto[i].telefono << endl;
+        cout << "Email: " << contacto[i].email << endl;
+        cout << "Nacionalidad: " << contacto[i].nacionalidad << endl;
     }
     return 0;
 }
